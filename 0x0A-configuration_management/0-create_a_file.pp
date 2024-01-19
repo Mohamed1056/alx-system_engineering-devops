1f1650548b8d7179464688b988c1f9857fb4161d
Mohamed Gamal <mg8033667@gmail.com>
2024-01-19 14:30:54 -0800

    adding creating file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,7 +2,7 @@
 
 file { '/tmp/school':
   content => 'I love puppet',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
