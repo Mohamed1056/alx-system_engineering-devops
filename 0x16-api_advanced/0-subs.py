613645b1b518fb0861657e5a33be79a17eae0a4b
Mohamed Gamal <mg8033667@gmail.com>
2024-03-07 14:52:53 -0800

    adding 1st file.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,9 +10,7 @@ def number_of_subscribers(subreddit):
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        results = response.json()
-        subs = results.get('data', {}).get('subscribers', 0)
-        return subs
-    else:
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
