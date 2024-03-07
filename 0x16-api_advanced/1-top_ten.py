91e5651b832f0c17df8b1f05b3f3bce8d43a80f1
Mohamed Gamal <mg8033667@gmail.com>
2024-03-07 14:28:08 -0800

    adding 2nd file.

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""Script for returning the hotest 10 posts."""
-
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    '''function to return the hotest 10 possts.'''
+    """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     params = {
-            "limit": 10
+        "limit": 10
     }
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
@@ -19,4 +18,4 @@ def top_ten(subreddit):
         print("None")
         return
     results = response.json().get("data")
-    [print(c.get("data").get("title")) forc in results.get("childern")]
+    [print(c.get("data").get("title")) for c in results.get("children")]
