9d43fbb5e530d9bd9de05b60b0454c8c8b09d463
Mohamed Gamal <mg8033667@gmail.com>
2024-03-07 14:09:50 -0800

    adding some files.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,5 +12,5 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
-    results = response.json().get("data")
+    results = response.json().["data"]
     return results.get("subscribers")
